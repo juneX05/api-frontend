c4407bce19f9f0456816da72a53aec31953d5f39
juneX05 <joelvankibona@gmail.com>
2020-04-12 03:38:59 +0300

    For Deployment Purposes
    We are working on this to ensure that we can deploy successfully on heroku

---
+++ dist/js/chunk-2d0c0301.ef68e7ee.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c0301"],{"415c":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("The main dashboard.")])])}],a=t("2877"),u={},l=Object(a["a"])(u,c,r,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0c0301.ef68e7ee.js.map
